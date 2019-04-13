9ae4bb8efbdaf7673746352e7489b5e90087012d
Debajyoti Dutta <debajyotidofficial@gmail.com>
2019-04-13 20:35:59 +0530

    Added the function to update the database.

--- src/table.py
+++ src/table.py
@@ -24,13 +24,22 @@ def display_data():
         print(d[1], end=" ")
         print(d[3])
 
+def update_data(ID, Description):
+    with conn:
+        c.execute("""UPDATE memo SET Description = :Description
+                    WHERE ID = :ID""",
+                  {'ID': ID, 'Description': Description})
+
+def update_dt(ID, Dt):
+    with conn:
+        c.execute("""UPDATE memo SET Dt = :Dt
+                    WHERE ID = :ID""",
+                  {'ID': ID, 'Dt': Dt})         
+
+
 def delete_data(ID):
     with conn:
         c.execute("DELETE from memo WHERE ID = :ID", {'ID': ID}) 
-
-def delete_all():
-    with conn:
-        c.execute("DELETE from memo") 
 
 def sort_ascending():
     sort_data=[]
@@ -54,7 +63,7 @@ def sort_descending():
         print(d)
 
 def menu():
-    # create()
+    #create()
     while True:
         Description=str(input("Enter the description: "))
         Remind = int(input("Do you want to Remind. Press 0 for No and Press 1 for Yes: "))
@@ -68,7 +77,34 @@ def menu():
             insert_data(Description, Remind, Dt)
         elif(Remind==0):
             insert_data(Description, Remind)
-        choice=int(input("Do you want to insert more. Press 0 for No and Press 1 for Yes: "))
+        wish=int(input("Do you want to Update. Press 0 for No and Press 1 for Yes: "))
+        if(wish==1):
+            k=int(input("Press 0 for Update Description and Press 1 for Update Date&Time and Press 2 for Update Both: "))
+            if(k==0):
+                ID=int(input("Enter the ID to Update: "))
+                Description=str(input("Enter the description: "))
+                update_data(ID, Description)
+            elif(k==1):
+                ID=int(input("Enter the ID to Update: "))
+                date=int(input("Enter the date: "))
+                month=int(input("Enter the month: "))
+                year=int(input("Enter the year: "))
+                hour=int(input("Enter the hour: "))
+                minute=int(input("Enter the min: "))
+                Dt = datetime.datetime(year, month, date, hour, minute, 0)
+                update_dt(ID, Dt)
+            elif(k==2):
+                ID=int(input("Enter the ID to Update: "))
+                Description=str(input("Enter the description: "))
+                date=int(input("Enter the date: "))
+                month=int(input("Enter the month: "))
+                year=int(input("Enter the year: "))
+                hour=int(input("Enter the hour: "))
+                minute=int(input("Enter the min: "))
+                Dt = datetime.datetime(year, month, date, hour, minute, 0)
+                update_data(ID, Description)
+                update_dt(ID, Dt)
+        choice=int(input("Do you want to Insert more. Press 0 for No and Press 1 for Yes: "))
         if(choice==0):
             break
 
@@ -78,8 +114,6 @@ def menu():
     display_data()
     sort_ascending()
     sort_descending()
-    delete_all()
-    display_data()
     conn.close()
 
 menu()
