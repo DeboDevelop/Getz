bedaa327b515bc5ca10f62598c15159748370596
Debajyoti Dutta <debajyotidofficial@gmail.com>
2019-04-13 15:39:08 +0530

    Add the function of delete all.

--- src/table.py
+++ src/table.py
@@ -3,47 +3,84 @@ import sqlite3
 
 def create():
     c.execute("""CREATE TABLE memo (
-        ID Integer PRIMARY KEY AUTOINCREMENT,
-        Description text,
-        Dt integer
-        )""")
-conn = sqlite3.connect("project.db")
+                ID Integer PRIMARY KEY AUTOINCREMENT,
+                Description text,
+                Remind integer,
+                Dt integer
+                )""")
+    
+conn = sqlite3.connect("projects.db")
 
 c = conn.cursor()
 
-def insert_data(description, Dt):
+def insert_data(Description, Remind, Dt=None):
     with conn:
-        c.execute("INSERT INTO memo(Description, Dt) VALUES (:Description, :Dt)", {'Description': description, 'Dt': Dt})
+        c.execute("INSERT INTO memo(Description, Remind, Dt) VALUES (:Description, :Remind, :Dt)", {'Description': Description, 'Remind':Remind, 'Dt': Dt})
 
-def display_data(D="World"):
-    c.execute("SELECT ID FROM memo WHERE Description=:Description", {'Description': D})
-    ch=c.fetchall()
-    print(ch)
-
+def display_data():
+    c.execute("SELECT * FROM memo")
+    data=c.fetchall()
+    for d in data:
+        print(d[1], end=" ")
+        print(d[3])
 
 def delete_data(ID):
     with conn:
         c.execute("DELETE from memo WHERE ID = :ID", {'ID': ID}) 
 
+def delete_all():
+    with conn:
+        c.execute("DELETE from memo") 
+
+def sort_ascending():
+    sort_data=[]
+    c.execute("SELECT * FROM memo")
+    data=c.fetchall()
+    for d in data:
+        sort_data.append(d[1])
+    sort_data.sort()
+    for d in sort_data:
+        print(d)
+
+def sort_descending():
+    sort_data=[]
+    c.execute("SELECT * FROM memo")
+    data=c.fetchall()
+    for d in data:
+        sort_data.append(d[1])
+    sort_data.sort()
+    sort_data.reverse()
+    for d in sort_data:
+        print(d)
+
 def menu():
-    #create()
+    # create()
     # while True:
-    #     description=str(input("Enter the description: "))
-    #     date=int(input("Enter the date: "))
-    #     month=int(input("Enter the month: "))
-    #     year=int(input("Enter the year: "))
-    #     hour=int(input("Enter the hour: "))
-    #     minute=int(input("Enter the min: "))
-    #     Dt = datetime.datetime(year, month, date, hour, minute, 0)
-    #     insert_data(description, Dt)
-    #     choice=int(input("Do you want to insert more. Please 0 for No and anything else for Yes: "))
+    #     Description=str(input("Enter the description: "))
+    #     Remind = int(input("Do you want to Remind. Press 0 for No and Press 1 for Yes: "))
+    #     if(Remind==1):
+    #         date=int(input("Enter the date: "))
+    #         month=int(input("Enter the month: "))
+    #         year=int(input("Enter the year: "))
+    #         hour=int(input("Enter the hour: "))
+    #         minute=int(input("Enter the min: "))
+    #         Dt = datetime.datetime(year, month, date, hour, minute, 0)
+    #         insert_data(Description, Remind, Dt)
+    #     elif(Remind==0):
+    #         insert_data(Description, Remind)
+    #     choice=int(input("Do you want to insert more. Press 0 for No and Press 1 for Yes: "))
     #     if(choice==0):
     #         break
 
-    #del_id=int(input("Enter the id to delete: "))
-    #delete_data(del_id)
+    # del_id=int(input("Enter the id to delete: "))
+    # delete_data(del_id)
 
+    display_data()
+    sort_ascending()
+    sort_descending()
+    delete_all()
     display_data()
     conn.close()
 
 menu()
+
